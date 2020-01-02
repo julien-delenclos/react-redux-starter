66aabc3638da7de6320d0215ee011d6604bd008e
julien-delenclos <59362955+julien-delenclos@users.noreply.github.com>
2020-01-02 14:41:34 +0100

    Update App.component.js

--- src/components/App/App.component.js
+++ src/components/App/App.component.js
@@ -5,6 +5,24 @@ import {Container, Row, Col} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends React.Component{
+  
+  constructor(){
+    super(...arguments)
+    this.state = {}
+  }
+  
+  componentDidUpdate(prevProps){
+    Object.keys(prevProps).forEach(key => {
+      if(prevProps[key] !== this.props[key]){
+        let oldValue = prevProps[key]
+        let value = this.props[key]
+        if(typeof this[`on${key[0].toUpperCase()}${key.slice(1)}Changed`] === 'function'){
+          this[`on${key[0].toUpperCase()}${key.slice(1)}Changed`](oldValue, value)
+        }
+      }
+    })
+  }
+  
   render(){
     return (
       <div className={Style.App + ' App'}>
